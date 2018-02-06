837842a6376d1b1903d01e45d66cd0acaf271fbb
willbrom <alisajid552@yahoo.com>
2018-02-06 14:00:33 +0300

    added layout for land orientation

--- app/src/main/java/com/willbrom/forismaticquotes/MainActivity.java
+++ app/src/main/java/com/willbrom/forismaticquotes/MainActivity.java
@@ -1,5 +1,7 @@
 package com.willbrom.forismaticquotes;
 
+import android.graphics.Typeface;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,9 +20,11 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements NetworkUtils.VollyCallbackListener {
 
-    private int QuoteKeyNumber = 1;
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String QUOTE_KEY = "quote_key";
+    private static final String QUOTE_AUTHOR_KEY = "quote_author_key";
     private ArrayList<String> quoteData = new ArrayList<>();
+    @BindView(R.id.title_textView) TextView titleTextView;
     @BindView(R.id.quoteText_textView) TextView quoteTextView;
     @BindView(R.id.quoteAuthor_textView) TextView quoteAuthorTextView;
 
@@ -29,19 +33,37 @@ public class MainActivity extends AppCompatActivity implements NetworkUtils.Voll
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        quoteTextView.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/Merienda-Bold.ttf"));
+        quoteAuthorTextView.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/Rancho-Regular.ttf"));
+        titleTextView.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/AmaticSC-Bold.ttf"));
+
+        if (savedInstanceState != null) {
+            quoteTextView.setText(savedInstanceState.getString(QUOTE_KEY));
+            quoteAuthorTextView.setText(savedInstanceState.getString(QUOTE_AUTHOR_KEY));
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString(QUOTE_KEY, quoteTextView.getText().toString());
+        outState.putString(QUOTE_AUTHOR_KEY, quoteAuthorTextView.getText().toString());
+        super.onSaveInstanceState(outState);
     }
 
     public void onClickNextQuote(View view) {
-        URL url = NetworkUtils.getQuoteUrl(String.valueOf(QuoteKeyNumber));
+        URL url = NetworkUtils.getQuoteUrl("");
         NetworkUtils.getHttpResponse(this, this, url);
         Log.d(TAG, url.toString());
-        QuoteKeyNumber++;
     }
 
     private void displayQuote() {
         if (quoteData != null) {
             quoteTextView.setText(quoteData.get(0));
-            quoteAuthorTextView.setText(quoteData.get(1));
+            if (!quoteData.get(1).equals(""))
+                quoteAuthorTextView.setText(quoteData.get(1));
+            else
+                quoteAuthorTextView.setText("Unknown");
         }
     }
 
